fb630f1cb16a094169d6ee78c03061f3ab445d10
Brian Holt <bharrisonholt@gmail.com>
2024-11-12 11:43:24 -0500

    first commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+// @ts-check
+
+import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
